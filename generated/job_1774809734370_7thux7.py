30082e004f4db677068dc6a1db2c4da29289a3ed
Divye Chandna <120579874+DivyeC@users.noreply.github.com>
2026-03-30 00:12:17 +0530

    Add Manim script for job_1774809734370_7thux7

---
+++ generated/job_1774809734370_7thux7.py
@@ -0,0 +1,31 @@
+from manim import *
+
+class Main(Scene):
+    def construct(self):
+        axes = Axes(
+            x_range=[-PI, PI, PI/2],
+            y_range=[-1.5, 1.5, 1],
+            x_length=10,
+            y_length=4,
+            axis_config={"include_tip": True}
+        ).add_coordinates()
+
+        labels = axes.get_axis_labels(x_label="x", y_label="y")
+
+        sine_graph = axes.plot(lambda x: np.sin(x), color=BLUE)
+        sine_label = MathTex("y = \\sin(x)", color=BLUE).next_to(sine_graph, UP, buff=0.5)
+
+        cosine_graph = axes.plot(lambda x: np.cos(x), color=RED)
+        cosine_label = MathTex("y = \\cos(x)", color=RED).next_to(cosine_graph, UP, buff=0.5)
+
+        self.play(Create(axes), Write(labels))
+        self.wait(0.5)
+        self.play(Create(sine_graph), Write(sine_label))
+        self.wait(1)
+
+        self.play(
+            ReplacementTransform(sine_graph, cosine_graph),
+            ReplacementTransform(sine_label, cosine_label),
+            run_time=2
+        )
+        self.wait(2)
