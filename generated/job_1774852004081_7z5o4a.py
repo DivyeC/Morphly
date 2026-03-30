ad8d3e4df753f33a15b012971bca52f724fad613
Divye Chandna <120579874+DivyeC@users.noreply.github.com>
2026-03-30 11:56:46 +0530

    Add Manim script for job_1774852004081_7z5o4a

---
+++ generated/job_1774852004081_7z5o4a.py
@@ -0,0 +1,26 @@
+from manim import *
+
+class Main(Scene):
+    def construct(self):
+        axes = Axes(
+            x_range=[-3, 3, 1],
+            y_range=[-1.5, 1.5, 1],
+            axis_config={"color": BLUE}
+        )
+
+        sine_graph = axes.plot(lambda x: np.sin(x), color=YELLOW)
+        cosine_graph = axes.plot(lambda x: np.cos(x), color=RED)
+
+        sine_label = MathTex("y = \\sin(x)").set_color(YELLOW).next_to(sine_graph, UP)
+        cosine_label = MathTex("y = \\cos(x)").set_color(RED).next_to(cosine_graph, UP)
+
+        self.play(Create(axes))
+        self.play(Create(sine_graph), Write(sine_label))
+        self.wait(1)
+
+        self.play(
+            Transform(sine_graph, cosine_graph),
+            Transform(sine_label, cosine_label),
+            run_time=2
+        )
+        self.wait(2)
